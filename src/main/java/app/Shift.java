5d249fdad3559073f5e3a38fe21eca24ad49cb86
Gio Odusanya <150759285+NotBigSurprise0@users.noreply.github.com>
2026-03-13 23:22:40 -0600

    Implemented last two constructors for Shift.

--- src/main/java/app/Shift.java
+++ src/main/java/app/Shift.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 
 public class Shift
 {
+    private static final String DEFAULT_NAME = "Unnamed";
+
     private static int nextId = 1;
 
     private String name;
@@ -44,7 +46,7 @@ public class Shift
     /**
      * Creates a new shift for a job from the given start and end time.
      * <p>
-     * The name of the shift will be a generic 
+     * The name of the shift will be Unnamed.
      * 
      * @param job The job the shift is for (cannot be {@code null})
      * @param start The start time of the shift (cannot be {@code null}) (cannot be after {@code end})
@@ -53,6 +55,47 @@ public class Shift
      */
     public Shift(Job job, LocalDateTime start, LocalDateTime end)
     {
-        this("Unnamed", job, start, end);
+        this(Shift.DEFAULT_NAME, job, start, end);
+    }
+
+    /**
+     * Creates a new shift for a job from the given start and duration with the given name.
+     * 
+     * @param name The name (identifier) for the shift (cannot be {@code null})
+     * @param job The job the shift is for (cannot be {@code null})
+     * @param start The start time of the shift (cannot be {@code null})
+     * @param duration The duration of the shift (cannot be {@code null}) (cannot be negative)
+     * @throws IllegalArgumentException If any argument is {@code null} or {@code duration} is negative
+     */
+    public Shift(String name, Job job, LocalDateTime start, Duration duration)
+    {
+        if (name == null || job == null || start == null || duration == null) throw new IllegalArgumentException("Arguments cannot be null.");
+        if (duration.isNegative()) throw new IllegalArgumentException("Duration cannot be negative.");
+
+        LocalDateTime calculatedEnd = start.plus(duration);
+
+        this.name = name;
+        this.id = Shift.nextId;
+        this.job = job;
+        this.start = start;
+        this.end = calculatedEnd;
+        this.duration = duration;
+        this.hourlyPay = job.getCurrentHourlyPay();
+        Shift.nextId++;
+    }
+
+    /**
+     * Creates a new shift for a job from the given start and duration.
+     * <p>
+     * The name of the shift will be Unnamed.
+     * 
+     * @param job The job the shift is for (cannot be {@code null})
+     * @param start The start time of the shift (cannot be {@code null}) (cannot be after {@code end})
+     * @param duration The duration of the shift (cannot be {@code null}) (cannot be negative)
+     * @throws IllegalArgumentException If any argument is {@code null} or {@code duration} is negative
+     */
+    public Shift(Job job, LocalDateTime start, Duration duration)
+    {
+        this(Shift.DEFAULT_NAME, job, start, duration);
     }
 }
