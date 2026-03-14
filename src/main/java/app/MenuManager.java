0d0f6c2bdb9c586e27441122ad17a4da953a6298
Gio Odusanya <150759285+NotBigSurprise0@users.noreply.github.com>
2026-03-14 13:51:59 -0600

    Added MenuManager class.

---
+++ src/main/java/app/MenuManager.java
@@ -0,0 +1,14 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuManager
+{
+    private List<Shift> currentShifts;
+
+    public MenuManager()
+    {
+        this.currentShifts = new ArrayList<>();
+    }
+}
