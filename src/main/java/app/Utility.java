fd50233c45932535986abb39ae9a721b6a404c44
Gio Odusanya <150759285+NotBigSurprise0@users.noreply.github.com>
2026-04-02 19:08:05 -0600

    Updated Utility methods checking if a string represents an integer to all use a method that checks if a string is an integer and satisfies a given condition with the integer as the input. Also added plain isInteger method.

--- src/main/java/app/Utility.java
+++ src/main/java/app/Utility.java
@@ -2,6 +2,7 @@ package app;
 
 import java.time.Duration;
 import java.util.Objects;
+import java.util.function.Function;
 
 public class Utility 
 {
@@ -31,6 +32,7 @@ public class Utility
      * 
      * @param duration The duration to format
      * @return The duration formatted as a String
+     * @throws NullPointerException If {@code duration} is {@code null}
      */
     public static String formatDuration(Duration duration)
     {
@@ -73,21 +75,26 @@ public class Utility
     }
 
     /**
-     * Determines if a String represents an integer and is within {@code min} and {@code max} inclusive on both ends.
+     * Determines if a String represents an integer and satisfyies the given condition.
+     * <p>
+     * Satisfying the condition means the function return true when applied to the integer.
      * 
-     * @param str The String to check
-     * @param min The minimum value that is considered valid for the number
-     * @param max The maximum value that is considered valid for the number
-     * @return {@code true} if {@code str} is an integer and is between {@code min} and {@code max}, otherwise {@code false}
+     * @param str The String to check (cannot be {@code null})
+     * @param condition The condition that returns true when the integer is considered valid and false otherwise (can be {@code null} to not check any condition)
+     * @return {@code true} if {@code str} is an integer and (satisfies {@code condition} or condition is {@code null}), otherwise {@code false}
+     * @throws NullPointerException If {@code str} is {@code null}
      */
-    public static boolean isIntegerInBounds(String str, int min, int max)
+    private static boolean isIntegerSatisfyingCondition(String str, Function<Integer, Boolean> condition)
     {
-        Objects.requireNonNull(str, "str cannot be null");
+        Objects.requireNonNull(str, "Str cannot be null");
 
         try
         {
             int num = Integer.parseInt(str.strip());
-            return num >= min && num <= max;
+            if (condition != null)
+                return condition.apply(num);
+            else
+                return true;
         }
         catch (NumberFormatException n)
         {
@@ -96,26 +103,54 @@ public class Utility
     }
 
     /**
+     * Determines if a String represents an integer and is within {@code min} and {@code max} inclusive on both ends.
+     * 
+     * @param str The String to check (cannot be {@code null})
+     * @param min The minimum value that is considered valid for the number
+     * @param max The maximum value that is considered valid for the number
+     * @return {@code true} if {@code str} is an integer and is between {@code min} and {@code max}, otherwise {@code false}
+     * @throws NullPointerException If {@code str} is {@code null}
+     */
+    public static boolean isIntegerInBounds(String str, int min, int max)
+    {
+        return isIntegerSatisfyingCondition(str, num -> num >= min && num <= max);
+    }
+
+    /**
      * Determines if a String represents an integer and is greater than or equal to {@code min}
      * 
-     * @param str The String to check
+     * @param str The String to check (cannot be {@code null})
      * @param min The minimum value that is considered valid for the number
      * @return {@code true} if {@code str} is an integer and is greater than or equal to {@code min}, otherwise {@code false}
+     * @throws NullPointerException If {@code str} is {@code null}
      */
     public static boolean isIntegerAtLeast(String str, int min)
     {
-        return isIntegerInBounds(str, min, Integer.MAX_VALUE);
+        return isIntegerSatisfyingCondition(str, num -> num >= min);
     }
 
     /**
      * Determines if a String represents an integer and is less than or equal to {@code max}
      * 
-     * @param str The String to check
+     * @param str The String to check (cannot be {@code null})
      * @param max The maximum value that is considered valid for the number
      * @return {@code true} if {@code str} is an integer and is less than or equal to {@code max}, otherwise {@code false}
+     * @throws NullPointerException If {@code str} is {@code null}
      */
     public static boolean isIntegerAtMost(String str, int max)
     {
-        return isIntegerInBounds(str, Integer.MIN_VALUE, max);
+        return isIntegerSatisfyingCondition(str, num -> num <= max);
+    }
+
+    /**
+     * Determines if a String represents an integer.
+     * 
+     * @param str The String to check (cannot be {@code null})
+     * @return {@code true} if {@code str} is an integer, otherwise {@code false}
+     * @throws NullPointerException If {@code str} is {@code null}
+     */
+    public static boolean isInteger(String str)
+    {
+        return isIntegerSatisfyingCondition(str, null);
     }
 }
